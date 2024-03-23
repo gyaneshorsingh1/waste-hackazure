22c26668edbb3a4ddb67fa7046e9b04f3481c45d
gyaneshorsingh1 <gyaneshorsingh9@gmail.com>
2024-03-23 12:35:02 +0530

    cmit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ html_template = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Computer Vision Output</title>
+    <title>Computer Vision Output</title> 
 </head>
 <body>
     <h1>Computer Vision Output</h1>
